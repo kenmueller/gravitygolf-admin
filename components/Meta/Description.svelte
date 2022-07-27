e84833a9ec8635f9857ae4389901e1ab5d36de30
Ken Mueller <kenmueller0@gmail.com>
2022-07-27 11:56:14 -0700

    initial commit

---
+++ components/Meta/Description.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let value = 'Gravity Golf Admin'
+</script>
+
+<svelte:head>
+	<meta name="description" content={value} />
+	<meta property="og:description" content={value} />
+	<meta name="twitter:description" content={value} />
+</svelte:head>
---
+++ routes/manifest.webmanifest.ts
@@ -0,0 +1,42 @@
+import type { RequestHandler } from '@sveltejs/kit'
+import type { WebAppManifest } from 'web-app-manifest'
+
+import errorFromValue from '$lib/error/from/value'
+
+import touch from '../images/touch.png'
+import mask from '../images/mask.png'
+
+const manifest: WebAppManifest = {
+	dir: 'ltr',
+	lang: 'en-US',
+	scope: '/',
+	start_url: '/',
+	name: 'Gravity Golf Admin',
+	short_name: 'Gravity Golf Admin',
+	description: 'Gravity Golf Admin',
+	display: 'standalone',
+	theme_color: '#24292e',
+	background_color: '#24292e',
+	categories: ['games', 'entertainment', 'education'],
+	icons: [
+		{ src: touch, sizes: '180x180' },
+		{ src: mask, sizes: '512x512', purpose: 'monochrome maskable' }
+	]
+}
+
+let data: string | null = null
+
+export const GET: RequestHandler = () => {
+	try {
+		return {
+			headers: {
+				'cache-control': 'no-cache',
+				'content-type': 'application/manifest+json'
+			},
+			body: (data ??= JSON.stringify(manifest))
+		}
+	} catch (value) {
+		const { code, message } = errorFromValue(value)
+		return { status: code, body: message }
+	}
+}
